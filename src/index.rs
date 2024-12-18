00be81751518ebb70de6e8f09d629345ca17fe24
Justus Magin <keewis@posteo.de>
2024-12-18 15:07:52 +0100

    make progress on the index implementation

--- src/index.rs
+++ src/index.rs
@@ -1,18 +1,39 @@
-use geoarrow::ArrayBase;
-use rstar::{RTree, RTreeObject};
+use geo::Polygon;
+use geoarrow::{
+    array::PolygonArray, scalar::Polygon as GeoArrowPolygon, trait_::ArrayAccessor,
+    trait_::NativeScalar,
+};
+use pyo3::prelude::*;
+use rstar::{primitives::CachedEnvelope, RTree, RTreeObject};
 
-pub struct Index<T>
-where T: RTreeObject {
-    rtree: RTree<T>;
+trait RStarRTree<T: RTreeObject> {
+    fn create_rstar_rtree(self) -> RTree<T>;
+}
+
+impl RStarRTree<CachedEnvelope<Polygon>> for PolygonArray {
+    fn create_rstar_rtree(self) -> RTree<CachedEnvelope<Polygon>> {
+        let cells: Vec<_> = self
+            .iter()
+            .flatten()
+            .map(|cell| {
+                let geom: Polygon = cell.to_geo();
+                CachedEnvelope::<Polygon>::new(geom.into())
+            })
+            .collect();
+
+        return RTree::bulk_load_with_params(cells);
+    }
+}
+
+pub struct Index {
+    rtree: RTree<CachedEnvelope<Polygon>>,
 }
 
 impl Index {
-    fn create(geoms: PolygonArray) -> Self {
-        let rtree = geoms.create_rtree();
+    fn create(cell_geoms: PolygonArray) -> Self {
+        let rtree = cell_geoms.create_rstar_rtree();
 
-        return Index {
-            rtree: rtree,
-        };
+        return Index { rtree: rtree };
     }
 }
 
@@ -20,15 +41,15 @@ impl Index {
 mod tests {
     use super::*;
 
-    use geoarrow::array::PointArray;
     use geo;
     use geo::Dimension;
+    use geoarrow::array::PolygonArray;
 
     #[test]
-    fn create_from_point_array() {
-        let point1 = geo::point!(x: 30, y: 20);
-        let point2 = geo::point!(x: 10, y: -50);
-        let array: PointArray = (vec![point1, point2].as_slice(), Dimension::XY).into();
+    fn create_from_polygon_array() {
+        let polygon1 = geo::polygon!(x: 30, y: 20);
+        let polygon2 = geo::polygon!(x: 10, y: -50);
+        let array: PolygonArray = (vec![polygon, polygon2].as_slice(), Dimension::XY).into();
 
         let index = Index::create(array);
     }
