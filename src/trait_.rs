3dd3a95847778c634d08005fa83bb45a931dbc67
Justus Magin <keewis@posteo.de>
2024-12-19 14:34:29 +0100

    depend on pyo3-geoarrow

--- src/trait_.rs
+++ src/trait_.rs
@@ -9,7 +9,7 @@ pub trait RStarRTree<T: RTreeObject> {
     fn create_rstar_rtree(self) -> RTree<T>;
 }
 
-impl RStarRTree<CachedEnvelope<Polygon>> for PolygonArray {
+impl RStarRTree<CachedEnvelope<Polygon>> for PolygonArray<8> {
     fn create_rstar_rtree(self) -> RTree<CachedEnvelope<Polygon>> {
         let cells: Vec<_> = self
             .iter()
