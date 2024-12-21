45f79edd9798cde3a5e9fff1d61d5a276a397f0d
Justus Magin <keewis@posteo.de>
2024-12-21 15:06:10 +0100

    remove the dummy function

--- src/lib.rs
+++ src/lib.rs
@@ -5,16 +5,9 @@ mod trait_;
 
 use self::index::Index;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
 /// A Python module implemented in Rust.
 #[pymodule]
 fn grid_indexing(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_class::<Index>()?;
     Ok(())
 }
