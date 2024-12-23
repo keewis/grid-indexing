92845bdca139023e4fad55b5aa050307d08da629
Justus Magin <keewis@posteo.de>
2024-12-23 21:00:09 +0100

    check that the overlap works as expected

--- python/tests/test_rtree.py
+++ python/tests/test_rtree.py
@@ -1,13 +1,11 @@
 import numpy as np
 import shapely
+import sparse
 
-from grid_indexing import Index
+from grid_indexing import Index, as_parts
 
 
-def test_create_index():
-    x = np.linspace(-10, 10, 6)
-    y = np.linspace(40, 60, 4)
-
+def create_cells(x, y):
     x_step = abs(x[1] - x[0]) / 2
     y_step = abs(y[1] - y[0]) / 2
 
@@ -22,11 +20,33 @@ def test_create_index():
         [[xmin, ymin], [xmin, ymax], [xmax, ymax], [xmax, ymin], [xmin, ymin]]
     )
     vertices = np.moveaxis(vertices_, (0, 1), (-2, -1))
-    polygons = shapely.polygons(vertices)
+    return shapely.polygons(vertices)
 
-    geom_type, coords, (ring_offsets, geom_offsets) = shapely.to_ragged_array(
-        polygons.flatten()
-    )
+
+def test_create_index():
+    x = np.linspace(-10, 10, 6)
+    y = np.linspace(40, 60, 4)
+
+    cells = create_cells(x, y).flatten()
+
+    type_, coords, (ring_offsets, geom_offsets) = shapely.to_ragged_array(cells)
 
     index = Index(coords, geom_offsets, ring_offsets)
     assert isinstance(index, Index)
+
+
+def test_query_overlap():
+    source_cells = create_cells(
+        np.linspace(-10, 10, 6), np.linspace(40, 60, 4)
+    ).flatten()
+    target_cells = source_cells
+
+    index = Index(*as_parts(source_cells))
+
+    actual = index.query_overlap(*as_parts(target_cells))
+
+    assert isinstance(actual, sparse.GCXS)
+    sum_ = np.sum(actual, axis=1).todense()
+    assert np.all(
+        np.isin(sum_, np.array([1, 4, 6, 9]))
+    ), "unexpected number of cells found"
