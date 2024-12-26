5d86ff6aa8470db62ca6dfa0039eadcf420a51a6
Justus Magin <keewis@posteo.de>
2024-12-27 00:08:37 +0100

    remove the unused `as_parts`

--- python/grid_indexing/__init__.py
+++ python/grid_indexing/__init__.py
@@ -1,6 +1,5 @@
 from grid_indexing import grid_indexing
 from grid_indexing.grid_indexing import Index  # noqa: F401
-from grid_indexing.util import as_parts  # noqa: F401
 
 __doc__ = grid_indexing.__doc__
 if hasattr(grid_indexing, "__all__"):
