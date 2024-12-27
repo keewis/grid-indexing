9d7a92bfcc43cdf3d16652f30b07cea07d08cf83
Justus Magin <keewis@posteo.de>
2024-12-27 17:09:41 +0100

    refactor to allow reusing the example datasets

--- python/tests/test_grids.py
+++ python/tests/test_grids.py
@@ -5,68 +5,70 @@ import xarray as xr
 from grid_indexing import grids
 
 
+def example_dataset(grid_type):
+    match grid_type:
+        case "1d-rectilinear":
+            lat = xr.Variable(
+                "lat", np.linspace(-10, 10, 3), {"standard_name": "latitude"}
+            )
+            lon = xr.Variable(
+                "lon", np.linspace(-5, 5, 4), {"standard_name": "longitude"}
+            )
+            ds = xr.Dataset(coords={"lat": lat, "lon": lon})
+        case "2d-rectilinear":
+            lat_, lon_ = np.meshgrid(np.linspace(-10, 10, 3), np.linspace(-5, 5, 4))
+            lat = xr.Variable(["y", "x"], lat_, {"standard_name": "latitude"})
+            lon = xr.Variable(["y", "x"], lon_, {"standard_name": "longitude"})
+            ds = xr.Dataset(coords={"lat": lat, "lon": lon})
+        case "2d-curvilinear":
+            lat_ = np.array([[0, 0, 0, 0], [1, 1, 1, 1], [2, 2, 2, 2]])
+            lon_ = np.array([[0, 1, 2, 3], [1, 2, 3, 4], [2, 3, 4, 5]])
+
+            lat = xr.Variable(["y", "x"], lat_, {"standard_name": "latitude"})
+            lon = xr.Variable(["y", "x"], lon_, {"standard_name": "longitude"})
+            ds = xr.Dataset(coords={"lat": lat, "lon": lon})
+        case "1d-unstructured":
+            lat = xr.Variable(
+                "cells", np.linspace(-10, 10, 12), {"standard_name": "latitude"}
+            )
+            lon = xr.Variable(
+                "cells", np.linspace(-5, 5, 12), {"standard_name": "longitude"}
+            )
+            ds = xr.Dataset(coords={"lat": lat, "lon": lon})
+        case "2d-crs":
+            data = np.linspace(-10, 10, 12).reshape(3, 4)
+            geo_transform = (
+                "101985.0 300.0379266750948 0.0 2826915.0 0.0 -300.041782729805"
+            )
+
+            attrs = {
+                "grid_mapping_name": "transverse_mercator",
+                "GeoTransform": geo_transform,
+            }
+
+            ds = xr.Dataset(
+                {"band_data": (["y", "x"], data)},
+                coords={"spatial_ref": ((), np.array(0), attrs)},
+            )
+
+    return ds
+
+
 class TestInferGridType:
-    def test_rectilinear_1d(self):
-        lat = xr.Variable("lat", np.linspace(-10, 10, 3), {"standard_name": "latitude"})
-        lon = xr.Variable("lon", np.linspace(-5, 5, 4), {"standard_name": "longitude"})
-        ds = xr.Dataset(coords={"lat": lat, "lon": lon})
-
+    @pytest.mark.parametrize(
+        "grid_type",
+        [
+            "1d-rectilinear",
+            "2d-rectilinear",
+            "2d-curvilinear",
+            "1d-unstructured",
+            "2d-crs",
+        ],
+    )
+    def test_infer_grid_type(self, grid_type):
+        ds = example_dataset(grid_type)
         actual = grids.infer_grid_type(ds)
-        assert actual == "1d-rectilinear"
-
-    def test_rectilinear_2d(self):
-        lat_, lon_ = np.meshgrid(np.linspace(-10, 10, 3), np.linspace(-5, 5, 4))
-        lat = xr.Variable(["y", "x"], lat_, {"standard_name": "latitude"})
-        lon = xr.Variable(["y", "x"], lon_, {"standard_name": "longitude"})
-        ds = xr.Dataset(coords={"lat": lat, "lon": lon})
-
-        actual = grids.infer_grid_type(ds)
-        assert actual == "2d-rectilinear"
-
-    def test_curvilinear_2d(self):
-        lat_ = np.array([[0, 0, 0, 0], [1, 1, 1, 1], [2, 2, 2, 2]])
-        lon_ = np.array([[0, 1, 2, 3], [1, 2, 3, 4], [2, 3, 4, 5]])
-
-        lat = xr.Variable(["y", "x"], lat_, {"standard_name": "latitude"})
-        lon = xr.Variable(["y", "x"], lon_, {"standard_name": "longitude"})
-        ds = xr.Dataset(coords={"lat": lat, "lon": lon})
-
-        actual = grids.infer_grid_type(ds)
-        assert actual == "2d-curvilinear"
-
-    def test_unstructured_1d(self):
-        lat = xr.Variable(
-            "cells", np.linspace(-10, 10, 12), {"standard_name": "latitude"}
-        )
-        lon = xr.Variable(
-            "cells", np.linspace(-5, 5, 12), {"standard_name": "longitude"}
-        )
-        ds = xr.Dataset(coords={"lat": lat, "lon": lon})
-
-        actual = grids.infer_grid_type(ds)
-
-        assert actual == "1d-unstructured"
-
-    def test_crs_2d(self):
-        data = np.linspace(-10, 10, 12).reshape(3, 4)
-        geo_transform = "101985.0 300.0379266750948 0.0 2826915.0 0.0 -300.041782729805"
-
-        ds = xr.Dataset(
-            {"band_data": (["y", "x"], data)},
-            coords={
-                "spatial_ref": (
-                    (),
-                    np.array(0),
-                    {
-                        "grid_mapping_name": "transverse_mercator",
-                        "GeoTransform": geo_transform,
-                    },
-                )
-            },
-        )
-
-        actual = grids.infer_grid_type(ds)
-        assert actual == "2d-crs"
+        assert actual == grid_type
 
     def test_missing_spatial_coordinates(self):
         ds = xr.Dataset()
