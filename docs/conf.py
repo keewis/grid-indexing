6f8694c6267b7226e469c132117148029d11c5fd
Justus Magin <keewis@posteo.de>
2025-06-11 23:12:26 +0200

    configure intersphinx and preprocess types

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,15 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 root_doc = "index"
 
 # -- Extension configuration -------------------------------------------------
+# intersphinx
+intersphinx_mapping = {
+    "dask": ("https://docs.dask.org/en/latest", None),
+    "numpy": ("https://numpy.org/doc/stable", None),
+    "python": ("https://docs.python.org/3/", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy", None),
+    "pydata-sparse": ("https://sparse.pydata.org/en/latest/", None),
+}
+
 # autosummary + autodoc
 autosummary_generate = True
 autodoc_typehints = "none"
@@ -33,6 +42,13 @@ napoleon_numpy_docstring = True
 napoleon_google_docstring = False
 napoleon_use_param = False
 napoleon_use_rtype = False
+napoleon_preprocess_types = True
+napoleon_type_aliases = {
+    "array-like": ":term:`array-like <array_like>`",
+    "arrow-array": ":term:`arrow-array`",
+    "sparse-array": ":term:`sparse-array`",
+    "chunked-sparse-array": ":term:`chunked-sparse-array`",
+}
 
 # myst
 myst_enable_extensions = [
