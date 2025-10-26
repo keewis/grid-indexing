390179734b04f5a57bddf6e16b46abc2382ed9a1
Justus Magin <keewis@posteo.de>
2025-10-26 14:52:54 +0100

    make `SphericalPoint` public

--- src/spherical/point.rs
+++ src/spherical/point.rs
@@ -3,7 +3,7 @@ use rstar::{Point, RTreeNum};
 use serde::{Deserialize, Serialize};
 
 #[derive(Copy, Clone, PartialEq, Debug, Deserialize, Serialize)]
-struct SphericalPoint {
+pub struct SphericalPoint {
     lon: f64,
     lat: f64,
 }
@@ -38,12 +38,12 @@ impl Point for SphericalPoint {
 
 impl SphericalPoint {
     /// Returns a new Point with all components set to zero.
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self::from_value(Zero::zero())
     }
 
     /// Applies `f` to each pair of components of `self` and `other`.
-    fn component_wise(
+    pub fn component_wise(
         &self,
         other: &Self,
         mut f: impl FnMut(<Self as Point>::Scalar, <Self as Point>::Scalar) -> <Self as Point>::Scalar,
@@ -52,7 +52,7 @@ impl SphericalPoint {
     }
 
     /// Returns whether all pairs of components of `self` and `other` pass test closure `f`. Short circuits if any result is false.
-    fn all_component_wise(
+    pub fn all_component_wise(
         &self,
         other: &Self,
         mut f: impl FnMut(<Self as Point>::Scalar, <Self as Point>::Scalar) -> bool,
@@ -61,7 +61,7 @@ impl SphericalPoint {
     }
 
     /// Returns the dot product of `self` and `rhs`.
-    fn dot(&self, rhs: &Self) -> <Self as Point>::Scalar {
+    pub fn dot(&self, rhs: &Self) -> <Self as Point>::Scalar {
         self.component_wise(rhs, |l, r| l * r)
             .fold(Zero::zero(), |acc, val| acc + val)
     }
@@ -73,52 +73,55 @@ impl SphericalPoint {
     /// The `start_value` is the value the accumulator will have on the first call of the closure.
     ///
     /// After applying the closure to every component of the Point, fold() returns the accumulator.
-    fn fold<T>(&self, start_value: T, mut f: impl FnMut(T, <Self as Point>::Scalar) -> T) -> T {
+    pub fn fold<T>(&self, start_value: T, mut f: impl FnMut(T, <Self as Point>::Scalar) -> T) -> T {
         (0..Self::DIMENSIONS).fold(start_value, |accumulated, i| f(accumulated, self.nth(i)))
     }
 
     /// Returns a Point with every component set to `value`.
-    fn from_value(value: <Self as Point>::Scalar) -> Self {
+    pub fn from_value(value: <Self as Point>::Scalar) -> Self {
         Self::generate(|_| value)
     }
 
     /// Returns a Point with each component set to the smallest of each component pair of `self` and `other`.
-    fn min_point(&self, other: &Self) -> Self {
+    pub fn min_point(&self, other: &Self) -> Self {
         self.component_wise(other, min_inline)
     }
 
     /// Returns a Point with each component set to the biggest of each component pair of `self` and `other`.
-    fn max_point(&self, other: &Self) -> Self {
+    pub fn max_point(&self, other: &Self) -> Self {
         self.component_wise(other, max_inline)
     }
 
     /// Returns the squared length of this Point as if it was a vector.
-    fn length_2(&self) -> <Self as Point>::Scalar {
+    pub fn length_2(&self) -> <Self as Point>::Scalar {
         self.fold(Zero::zero(), |acc, cur| cur * cur + acc)
     }
 
     /// Subtracts `other` from `self` component wise.
-    fn sub(&self, other: &Self) -> Self {
+    pub fn sub(&self, other: &Self) -> Self {
         self.component_wise(other, |l, r| l - r)
     }
 
     /// Adds `other` to `self` component wise.
-    fn add(&self, other: &Self) -> Self {
+    pub fn add(&self, other: &Self) -> Self {
         self.component_wise(other, |l, r| l + r)
     }
 
     /// Multiplies `self` with `scalar` component wise.
-    fn mul(&self, scalar: <Self as Point>::Scalar) -> Self {
+    pub fn mul(&self, scalar: <Self as Point>::Scalar) -> Self {
         self.map(|coordinate| coordinate * scalar)
     }
 
     /// Applies `f` to `self` component wise.
-    fn map(&self, mut f: impl FnMut(<Self as Point>::Scalar) -> <Self as Point>::Scalar) -> Self {
+    pub fn map(
+        &self,
+        mut f: impl FnMut(<Self as Point>::Scalar) -> <Self as Point>::Scalar,
+    ) -> Self {
         Self::generate(|i| f(self.nth(i)))
     }
 
     /// Returns the squared distance between `self` and `other`.
-    fn distance_2(&self, other: &Self) -> <Self as Point>::Scalar {
+    pub fn distance_2(&self, other: &Self) -> <Self as Point>::Scalar {
         self.sub(other).length_2()
     }
 }
